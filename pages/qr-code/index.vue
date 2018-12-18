24e85c3d0093b38264ab104ad20bdf0852a5a11c
chenjichuan <chenjichuan521@sina.cn>
2018-12-18 20:22:20 +0800

    bugfix

--- pages/qr-code/index.vue
+++ pages/qr-code/index.vue
@@ -1,14 +1,32 @@
 <template>
-  <div>
-    qr-code
+  <div class="mode-qr">
+    <Card style="width:50%; margin: 0 auto;">
+      <div style="text-align:center">
+        <qrcode-vue
+          :value="value"
+          size="400"
+          level="H" />
+      </div>
+    </Card>
+
   </div>
 </template>
 
 <script>
+  import QrcodeVue from 'qrcode.vue';
   export default {
+    components: {QrcodeVue},
+    data() {
+      return {
+        value: ''
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  .mode-qr {
+    text-align: center;
+  }
 
 </style>
