817365dda0b17f1a3135a5dff315f46d27d264fa
chenjichuan <chenjichuan521@sina.cn>
2018-12-27 02:15:23 +0800

    service worker

--- plugins/sw/index.js
+++ plugins/sw/index.js
@@ -1,17 +1,21 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw/sw.js').then(function(reg) {
+if(process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw/sw.js').then(function(reg) {
 
-    if(reg.installing) {
-      console.log('Service worker installing');
-    } else if(reg.waiting) {
-      console.log('Service worker installed');
-    } else if(reg.active) {
-      console.log('Service worker active');
-    }
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
 
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  }
 }
+
+
 
