730932116a27981823a5e312a27603ce5b9a3b92
chenjichuan <chenjichuan521@sina.cn>
2018-12-28 16:35:46 +0800

    readme

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="mod-home">
-    <!--<Logo class="logo" :class="tranclass" style="opacity: 0;"/>-->
     <footer class="footer">
       <a href="https://github.com/chenjichuan/fe-tools" target="_blank" class="github">
         <Icon type="logo-github" />
@@ -29,82 +28,5 @@
 </script>
 
 <style scoped lang="less">
-  .rotateIn {
-    opacity: 0;
-    animation: rotateIn 1s;
-    animation-fill-mode: forwards;
-  }
-
-  @-webkit-keyframes rotateIn {
-    from {
-      -webkit-transform-origin: center;
-      transform-origin: center;
-      -webkit-transform: rotate3d(0, 0, 1, -200deg);
-      transform: rotate3d(0, 0, 1, -200deg);
-      opacity: 0;
-    }
-
-    to {
-      -webkit-transform-origin: center;
-      transform-origin: center;
-      -webkit-transform: translate3d(0, 0, 0);
-      transform: translate3d(0, 0, 0);
-      opacity: 1;
-    }
-  }
-
-  @keyframes rotateIn {
-    from {
-      -webkit-transform-origin: center;
-      transform-origin: center;
-      -webkit-transform: rotate3d(0, 0, 1, -200deg);
-      transform: rotate3d(0, 0, 1, -200deg);
-      opacity: 0;
-    }
-
-    to {
-      -webkit-transform-origin: center;
-      transform-origin: center;
-      -webkit-transform: translate3d(0, 0, 0);
-      transform: translate3d(0, 0, 0);
-      opacity: 1;
-    }
-  }
-
-  .mod-home {
-    width: 100%;
-    position: relative;
-    height: 85vh;
-    overflow: auto;
-    /*display: flex;*/
-    /*align-items: center;*/
-    /*justify-content: center;*/
-    /*.logo {*/
-    /*transform: rotate3d(0, 0, 1, -200deg);*/
-    /*width: 500px;*/
-    /*border-radius: 22px;*/
-    /*overflow: hidden;*/
-    /*}*/
-    .footer {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      text-align: center;
-      .copyright {
-        color: rgba(0,0,0,.45);
-        font-size: 14px;
-      }
-      .github {
-        font-size: 30px;
-        margin-bottom: 8px;
-        display: block;
-        color: rgba(0,0,0,.45);
-        transition: all .3s;
-      }
-      .trans {
-        display: inline-block;
-        transform: rotate(180deg);
-      }
-    }
-  }
+  @import "./index";
 </style>
